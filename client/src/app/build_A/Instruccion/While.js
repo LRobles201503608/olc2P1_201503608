a2097b6361d7272e643863280cf1b75dd10a9297
201503608 <luisrobles2408@gmail.com>
2020-09-12 12:37:43 -0600

    añadiendo traduccion

---
+++ client/src/app/build_A/Instruccion/While.js
@@ -0,0 +1,69 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.While = void 0;
+const Node_1 = require("../Abstract/Node");
+const Table_1 = require("../Simbols/Table");
+const Errors_1 = require("../util/Errors");
+const Types_1 = require("../util/Types");
+const Continue_1 = require("../Expresiones/Continue");
+const Break_1 = require("../Expresiones/Break");
+/**
+ * @class Ejecuta una serie de instrucciones en caso la condicion sea verdadera sino ejecuta las instrucciones falsas
+ */
+class While extends Node_1.Node {
+    /**
+     * @constructor Crea el nodo instruccion para la sentencia IF
+     * @param condition Condicion que debe ser tipo boolean
+     * @param List Lista de instrucciones a ejecutar mientras la condicion sea verdadera
+     * @param line Linea de la sentencia while
+     * @param column Columna de la sentencia while
+     */
+    constructor(condition, List, line, column) {
+        super(null, line, column);
+        this.condition = condition;
+        this.List = List;
+        this.column = column;
+        this.line = line;
+    }
+    execute(table, tree) {
+        const newtable = new Table_1.Table(table);
+        let result;
+        result = this.condition.execute(newtable, tree);
+        if (result instanceof Errors_1.Error) {
+            return result;
+        }
+        if (this.condition.type.type !== Types_1.types.BOOLEAN) {
+            const error = new Errors_1.Error('Semantico', 'Se esperaba una expresion booleana para la condicion', this.line, this.column);
+            tree.errores.push(error);
+            tree.console.push(error.toString());
+            return error;
+        }
+        if (result) {
+            do {
+                result = this.condition.execute(newtable, tree);
+                if (result instanceof Errors_1.Error) {
+                    return result;
+                }
+                if (this.condition.type.type !== Types_1.types.BOOLEAN) {
+                    const error = new Errors_1.Error('Semantico', 'Se esperaba una expresion booleana para la condicion', this.line, this.column);
+                    tree.errores.push(error);
+                    tree.console.push(error.toString());
+                    return error;
+                }
+                if (result) {
+                    for (let i = 0; i < this.List.length; i++) {
+                        const res = this.List[i].execute(newtable, tree);
+                        if (res instanceof Continue_1.Continue) {
+                            break;
+                        }
+                        else if (res instanceof Break_1.Break) {
+                            return;
+                        }
+                    }
+                }
+            } while (result);
+        }
+        return null;
+    }
+}
+exports.While = While;
